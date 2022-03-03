e0b70fc17a516458f8253969d7a550dd0a9f49a0
Basil19 <bojsl-9@hotmail.com>
2022-03-03 05:11:23 +0300

    Initial commit ✨

---
+++ app/src/main/java/com/albasil/together/Home.kt
@@ -0,0 +1,54 @@
+package com.albasil.together
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.albasil.together.databinding.FragmentHomeBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class Home : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        binding.iconPhoto.setOnClickListener {
+            makeToast("Photo")
+        }
+
+        binding.iconText.setOnClickListener {
+            makeToast("Text")
+        }
+
+        binding.iconVideo.setOnClickListener {
+            makeToast("Video")
+        }
+    }
+
+    private fun makeToast(text:String) {
+        Toast.makeText(context, "this is $text", Toast.LENGTH_SHORT).show()
+    }
+
+
+}
